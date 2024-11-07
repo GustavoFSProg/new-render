6a980bc86512a009e00e2f7cf1f2c8dbcd4594bb
GustavoFSProg <gustavo.prog40@gmail.com>
2024-11-06 21:18:06 -0300

    sqlite deployhoje da

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import userController from './userController'
+import userController from './userController.js'
 
 dotenv.config()
 
